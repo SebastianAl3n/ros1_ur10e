fb09e1f73a637a544b7e5cca10cff9c6273a6be9
SebastianAl3n <alensebastian456@gmail.com>
2026-04-12 18:18:28 +0200

    3D pose detection added

--- src/ur10e_sim/src/pose_extraction.py
+++ src/ur10e_sim/src/pose_extraction.py
@@ -5,6 +5,9 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from robot_utils import RobotControl
+import tf2_ros
+import tf2_geometry_msgs
+from geometry_msgs.msg import PointStamped
 
 class ColorDetector:
     def __init__(self):
@@ -12,12 +15,24 @@ class ColorDetector:
         self.lower_green = np.array([35, 10, 10])
         self.upper_green = np.array([85, 255, 255])
         self.current_image = None
+        self.current_depth = None
 
         self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.callback)
+        self.depth_sub = rospy.Subscriber("/camera/depth/image_raw",Image,self.depth_callback)
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        rospy.sleep(1.0)
 
     def callback(self, data):
         try:
             self.current_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except Exception as e:
+            rospy.logerr(f"Error converting image: {e}")
+
+    def depth_callback(self, data):
+        try:
+            self.current_depth = self.bridge.imgmsg_to_cv2(data,"32FC1")
         except Exception as e:
             rospy.logerr(f"Error converting image: {e}")
     
@@ -32,8 +47,7 @@ class ColorDetector:
         if contours:
             largest_contour = max(contours, key=cv2.contourArea)
             if cv2.contourArea(largest_contour) > 50: 
-                # Get the bounding box coordinates
-                x, y, w, h = cv2.boundingRect(largest_contour)
+                x, y, w, h = cv2.boundingRect(largest_contour) #bounding box coordinate
                 
                 # Optional: Draw the box on the image for debugging
                 cv2.rectangle(self.current_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -45,6 +59,47 @@ class ColorDetector:
         cv2.waitKey(1)
         
         return bbox
+    
+    def get_3d_pose(self, cx, cy):
+        if self.current_depth is None:
+            rospy.logwarn("No Depth info recieved")
+            return None
+        
+        z_m = self.current_depth[cy,cx] # accessing the depth info at these pixels
+
+        if np.isnan(z_m) or z_m <=0:
+            rospy.logwarn("Invalid Depth Value at Centroid")
+
+        #calculating the xyz of the centroid in the camera frame
+
+        fx = 465.7
+        fy = 465.7
+        ox = 320 # center of 680 width
+        oy = 240  # center of 480 height
+
+        x_c = (cx - ox) * z_m / fx
+        y_c = (cy - oy) * z_m / fy
+        z_c = z_m
+
+        return (x_c, y_c, z_c)
+
+    def get_world_pose(self, cam_x, cam_y, cam_z):
+        try:
+            transform = self.tf_buffer.lookup_transform("world","camera_color_optical_frame",rospy.Time(0),rospy.Duration(1.0))
+
+            p_cam = PointStamped()
+            p_cam.header.frame_id = "camera_color_optical_frame"
+            p_cam.point.x = cam_x
+            p_cam.point.y = cam_y
+            p_cam.point.z = cam_z
+
+            p_world = tf2_geometry_msgs.do_transform_point(p_cam,transform)
+            return p_world.point
+
+        except Exception as e:
+            rospy.logerr(f"transformation failed: {e}")
+            return None
+
 
 
 
@@ -75,6 +130,26 @@ if __name__ == "__main__":
             center_x = x + (w // 2)
             center_y = y + (h // 2)
             rospy.loginfo(f"SUCCESS: Found green object at Center ({center_x}, {center_y})")
+
+            pose_cam = vision.get_3d_pose(center_x,center_y)
+
+            if pose_cam:
+                x_c, y_c, z_c = pose_cam
+                rospy.loginfo(f"Success..found object at Camera Frame X={x_c:.3f}m, Y={y_c:.3f}m, Z={z_c:.3f}m")
+
+                pose_world = vision.get_world_pose(x_c, y_c, z_c)
+
+                if pose_world:
+                    world_x = pose_world.x
+                    world_y = pose_world.y
+                    world_z = pose_world.z
+                    rospy.loginfo(f"Success..found object at World Frame X={world_x:.3f}m, Y={world_y:.3f}m, Z={world_z:.3f}m")
+
+                else:
+                    rospy.logwarn("Transformation was not successfull")
+
+            else:
+                rospy.logwarn("Target found in 2D but 3D Pose extraction failed")
             
             # This is where you'd add: robot.go_to_pose(...) to grab it!
         else:
